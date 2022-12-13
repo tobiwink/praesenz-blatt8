0f35cd58b62528105f9337f98f7332dbd1298154
Long Nguyen <long.pollehn@gmail.com>
2022-12-13 09:58:44 +0100

    Edited unit test a bit

--- test_bubblesort.py
+++ test_bubblesort.py
@@ -2,18 +2,18 @@ import unittest
 import bubblesort
 
 class TestBubbleSort(unittest.TestCase):
-    def test1(self):
+    def test_a_normal_array(self):
         input_array = [3, 2, 1]
         bubblesort.bubblesort(input_array)
-        self.assertEqual([1, 2, 3], input_array)  # add assertion here
-    def test2(self):
+        self.assertEqual([1, 2, 3], input_array)
+    def test_an_empty_array(self):
         input_array = []
         bubblesort.bubblesort(input_array)
-        self.assertEqual([], input_array)  # add assertion here
-    def test3(self):
+        self.assertEqual([], input_array)
+    def test_array_with_1_element(self):
         input_array = [1]
         bubblesort.bubblesort(input_array)
-        self.assertEqual([1], input_array)  # add assertion here
+        self.assertEqual([1], input_array)
 
 
 if __name__ == '__main__':
